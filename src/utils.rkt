33fa98eb548bdf04008f18d77e2cfa8744339dfd
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2016-12-22 14:58:03 +0000

    add utils

--- src/utils.rkt
+++ src/utils.rkt
@@ -5,6 +5,16 @@
 
 (define (open-file path-as-string)
   (file->string (string->path path-as-string)))
-  
 
+(define (open-file-as-list path-as-string)
+  (file->list (string->path path-as-string)))
+
+(define (type? x)
+  (cond
+    [number? "number"]
+    [string? "string"]
+    [list? "list"]
+    [byte? "byte"]
+    [else "unknown type"]))
+  
 (provide (all-defined-out))
